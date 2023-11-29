2a91f31eba22ccc32d0831325a18111790f066df
bruno-gds <brunog.damascena@gmail.com>
2023-11-29 19:43:37 -0300

    ajustando o atributo timestamp

--- src/main/java/fiap/testesnobackend/model/Mensagem.java
+++ src/main/java/fiap/testesnobackend/model/Mensagem.java
@@ -1,13 +1,16 @@
 package fiap.testesnobackend.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -36,9 +39,19 @@ public class Mensagem {
     @NotEmpty(message = "O conteúdo não pode estar vazio")
     private String conteudo;
 
-    @Builder.Default // PARA JÁ INICIAR COM ESTE VALOR
+//    @Builder.Default // PARA JÁ INICIAR COM ESTE VALOR
+    @CreationTimestamp // Carregar a data de criação de forma automatica
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSS")
     private LocalDateTime dataCriacaoMensagem = LocalDateTime.now();
 
     @Builder.Default
     private int gostei = 0;
+
+
+    @PrePersist
+    public void prePersist() {
+        var timestamp = LocalDateTime.now();
+
+        this.dataCriacaoMensagem = timestamp;
+    }
 }
