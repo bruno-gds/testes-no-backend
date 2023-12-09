d4a443f5423684fa59a189676b45f224975a87bf
bruno-gds <brunog.damascena@gmail.com>
2023-12-08 21:08:21 -0300

    incluindo mais testes para avaliar performance

--- src/test/java/fiap/testesnobackend/performance/PerformanceSimulation.java
+++ src/test/java/fiap/testesnobackend/performance/PerformanceSimulation.java
@@ -23,20 +23,47 @@ public class PerformanceSimulation extends Simulation {
             http.baseUrl("http://localhost:8080")
                     .header("Content-Type", "application/json");
 
-    ActionBuilder adicionarMensagemRequest = http("Adicionar Mensagem")
+    ActionBuilder adicionarMensagemRequest = http("request: adicionar mensagem")
             .post("/mensagens")
             .body(StringBody("{\"usuario\": \"user\", \"conteudo\": \"Conteudo da mensagem\"}"))
-            .check(status().is(201));
+            .check(status().is(201))
+            .check(jsonPath("$.id").saveAs("mensagemId"));
 
-    ScenarioBuilder cenarioAdicionarMensagem = scenario("Adicionar Mensagem")
+    ActionBuilder buscarMensagemRequest = http("request: buscar mensagem")
+            .get("/mensagens/#{mensagemId}")
+            .check(status().is(200));
+
+    ActionBuilder removeMensagemRequest = http("request: remover mensagem")
+            .delete("/mensagens/#{mensagemId}")
+            .check(status().is(200));
+
+    ScenarioBuilder cenarioAdicionarMensagem = scenario("adicionar mensagem")
             .exec(adicionarMensagemRequest);
+
+    ScenarioBuilder cenarioBuscarMensagem = scenario("buscar mensagem")
+            .exec(adicionarMensagemRequest)
+            .exec(buscarMensagemRequest);
+
+    ScenarioBuilder cenarioRemoverMensagem = scenario("remover mensagem")
+            .exec(adicionarMensagemRequest)
+            .exec(removeMensagemRequest);
 
     {
         setUp(
                 cenarioAdicionarMensagem.injectOpen(
+                        rampUsersPerSec(1).to(2).during(Duration.ofSeconds(10)),
+                        constantUsersPerSec(2).during(Duration.ofSeconds(20)),
+                        rampUsersPerSec(2).to(1).during(Duration.ofSeconds(10))
+                ),
+                cenarioBuscarMensagem.injectOpen(
                         rampUsersPerSec(1).to(10).during(Duration.ofSeconds(10)),
                         constantUsersPerSec(10).during(Duration.ofSeconds(20)),
                         rampUsersPerSec(10).to(1).during(Duration.ofSeconds(10))
+                ),
+                cenarioRemoverMensagem.injectOpen(
+                        rampUsersPerSec(1).to(5).during(Duration.ofSeconds(10)),
+                        constantUsersPerSec(5).during(Duration.ofSeconds(20)),
+                        rampUsersPerSec(5).to(1).during(Duration.ofSeconds(10))
                 )
         )
                 .protocols(httpProtocol)
